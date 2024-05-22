cc613bb9c5a83939a5635f383303d405a318e722
azwaans <antoine.zwaans@bsse.ethz.ch>
2024-05-22 14:24:12 +0200

    adjust figure 3 color consistency +  include only 3 skyline growth estimates

--- figure_3/plot_figure_3.R
+++ figure_3/plot_figure_3.R
@@ -87,11 +87,11 @@ plot1_4 <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Clu
 
 plot1_2 <-  plot1_2 + theme(text=element_text(size = text_size),panel.border = element_blank() , axis.line = element_line() , axis.ticks = element_blank(), axis.text = element_blank() )
 plot1_3 <-  plot1_3 + theme(text=element_text(size = text_size),panel.border = element_blank() , axis.line = element_line() , axis.ticks = element_blank(), axis.text = element_blank() )
-plot1_4 <-  plot1_4 + theme(text=element_text(size = text_size),panel.border = element_blank() , axis.line = element_line() , axis.ticks = element_blank(), axis.text = element_blank(),legend.position = c(0.1,0.1) )
+plot1_4 <-  plot1_4 + theme(text=element_text(size = text_size),panel.border = element_blank() , axis.line = element_line() , axis.ticks = element_blank(), axis.text = element_blank(),legend.position = c(0.5,0.5) )
 
-legend <- get_legend(plot1_4, position= "right") 
+legend <- get_legend(plot1_4) 
 
-row_1 <- cowplot::plot_grid(plot1_2+ theme(plot.title = element_blank()),plot1_3 + theme(plot.title = element_blank()),plot1_4+ theme(plot.title = element_blank(),legend.position = "none"),legend,nrow = 1,align = "h",label_size=text_size,rel_widths = c(1,1,1,1))
+row_1 <- cowplot::plot_grid(plot1_2+ theme(plot.title = element_blank()),plot1_3 + theme(plot.title = element_blank()),plot1_4+ theme(plot.title = element_blank(),legend.position = "none"),legend ,nrow = 1,align = "h",label_size=text_size,rel_widths = c(1,1,1,1))
 ggsave(paste0(pic_dir,"row_1.pdf"),
        row_1, width = 14.28, height = 7.14, units = "cm", dpi = 300)
 
@@ -138,12 +138,12 @@ ltt_upgma <- ltt.plot.coords(upgma)
 all_ltt_constant <- rbind(all_ltt_constant, cbind(ltt_upgma,rep(length(trees_constant)+1,nrow(ltt_upgma))))
 
 ##add the scaled upgma
-ltt_upgma_scaled_constant <- ltt.plot.coords(upgma_scaled_constant)
-all_ltt_constant <- rbind(all_ltt_constant, cbind(ltt_upgma_scaled_constant,rep(length(trees_constant)+2,nrow(ltt_upgma_scaled_constant))))
+#ltt_upgma_scaled_constant <- ltt.plot.coords(upgma_scaled_constant)
+#all_ltt_constant <- rbind(all_ltt_constant, cbind(ltt_upgma_scaled_constant,rep(length(trees_constant)+2,nrow(ltt_upgma_scaled_constant))))
 
 #MCC_constant
 ltt_MCC_constant <- ltt.plot.coords(MCC_constant)   
-all_ltt_constant <- rbind(all_ltt_constant, cbind(ltt_MCC_constant,rep(length(trees_constant)+3,nrow(ltt_MCC_constant))))
+all_ltt_constant <- rbind(all_ltt_constant, cbind(ltt_MCC_constant,rep(length(trees_constant)+2,nrow(ltt_MCC_constant))))
 
 all_ltt_constant <- data.frame(all_ltt_constant)
 
@@ -151,7 +151,10 @@ all_ltt_constant <- data.frame(all_ltt_constant)
 all_ltt_constant$time <- all_ltt_constant$time + 25
 
 #create another column to colour by type  
-all_ltt_constant$type <- c(rep("SciPhy constant",length(which(all_ltt_constant$number<=(length(trees_constant))))),rep("UPGMA origin",length(which(all_ltt_constant$number==(length(trees_constant)+1)))),rep("UPGMA root (constant)",length(which(all_ltt_constant$number==(length(trees_constant)+2)))),rep("SciPhy MCC",length(which(all_ltt_constant$number==(length(trees_constant)+3)))))
+#all_ltt_constant$type <- c(rep("SciPhy constant",length(which(all_ltt_constant$number<=(length(trees_constant))))),rep("UPGMA origin",length(which(all_ltt_constant$number==(length(trees_constant)+1)))),rep("UPGMA root (constant)",length(which(all_ltt_constant$number==(length(trees_constant)+2)))),rep("SciPhy MCC",length(which(all_ltt_constant$number==(length(trees_constant)+3)))))
+all_ltt_constant$type <- c(rep("SciPhy constant",length(which(all_ltt_constant$number<=(length(trees_constant))))),rep("UPGMA origin",length(which(all_ltt_constant$number==(length(trees_constant)+1)))),rep("SciPhy MCC",length(which(all_ltt_constant$number==(length(trees_constant)+2)))))
+
+
 cols <- c("SciPhy constant" = "#E1E1F7", "SciPhy MCC" = "black", "UPGMA origin" = "red","UPGMA root (constant)" = "#E07E5E")
 
 
@@ -196,12 +199,17 @@ ltt_upgma <- ltt.plot.coords(upgma)
 all_ltt <- rbind(all_ltt, cbind(ltt_upgma,rep(length(trees)+1,nrow(ltt_upgma))))
 
 ##add the scaled upgma
-ltt_upgma_scaled <- ltt.plot.coords(upgma_scaled)
-all_ltt <- rbind(all_ltt, cbind(ltt_upgma_scaled,rep(length(trees)+2,nrow(ltt_upgma_scaled))))
+##ltt_upgma_scaled <- ltt.plot.coords(upgma_scaled)
+##all_ltt <- rbind(all_ltt, cbind(ltt_upgma_scaled,rep(length(trees)+2,nrow(ltt_upgma_scaled))))
 
 #MCC
 ltt_mcc <- ltt.plot.coords(MCC)   
-all_ltt <- rbind(all_ltt, cbind(ltt_mcc,rep(length(trees)+3,nrow(ltt_mcc))))
+all_ltt <- rbind(all_ltt, cbind(ltt_mcc,rep(length(trees)+2,nrow(ltt_mcc))))
+
+###UPGMA + SCIPHY 
+MCC_UPGMA_SCALED <- ape::read.nexus(file = "/Users/azwaans/typewriter_analysis/results/analysis_cell_culture_data/fixed_tree_analyses/fixed_trees/1000_UPGMA_medianPosteriorHeight_estimateBranchLengths_nodeReheight_combined_thinned1000000_MCC.tree")
+ltt_mcc_upgma_scaled <- ltt.plot.coords(MCC_UPGMA_SCALED)   
+all_ltt <- rbind(all_ltt, cbind(ltt_mcc_upgma_scaled,rep(length(trees)+3,nrow(ltt_mcc_upgma_scaled))))
 
 all_ltt <- data.frame(all_ltt)
 
@@ -209,9 +217,13 @@ all_ltt <- data.frame(all_ltt)
 all_ltt$time <- all_ltt$time + 25
 
 
-all_ltt$type <- c(rep("SciPhy 13-epochs",length(which(all_ltt$number<=(length(trees))))),rep("UPGMA origin",length(which(all_ltt$number==(length(trees)+1)))),rep("UPGMA root (13-epochs)",length(which(all_ltt$number==(length(trees)+2)))),rep("SciPhy MCC",length(which(all_ltt$number==(length(trees)+3)))))
-cols <- c("SciPhy constant" = "#E1E1F7", "UPGMA origin" = "red","UPGMA root (constant)" = "#E07E5E","SciPhy MCC" = "black", "SciPhy 13-epochs" = "#56996E", "UPGMA" = "red","UPGMA root (13-epochs)" = "#E07E5E")
-alphas <- c("SciPhy constant" = 0.3, "UPGMA origin" = 1.0,"UPGMA root (constant)" = 1.0,"SciPhy MCC" = 1.0,"SciPhy 13-epochs" = 0.3, "UPGMA" = 1.0,"UPGMA root (13-epochs)" = 1.0)
+all_ltt$type <- c(rep("SciPhy dynamic",length(which(all_ltt$number<=(length(trees))))),rep("UPGMA origin",length(which(all_ltt$number==(length(trees)+1)))),rep("SciPhy MCC",length(which(all_ltt$number==(length(trees)+2)))),rep("UPGMA - SciPhy",length(which(all_ltt$number==(length(trees)+3)))))
+#all_ltt$type <- c(rep("SciPhy dynamic",length(which(all_ltt$number<=(length(trees))))),rep("UPGMA origin",length(which(all_ltt$number==(length(trees)+1)))),rep("SciPhy MCC",length(which(all_ltt$number==(length(trees)+2))),rep("UPGMA - SciPhy ",length(which(all_ltt$number==(length(trees)+3))))))
+
+
+cols <- c("SciPhy constant" ="#56996E", "UPGMA origin" = "red","UPGMA root (constant)" = "#E07E5E","SciPhy MCC" = "black", "SciPhy dynamic" =  "#E1E1F7", "UPGMA" = "red","UPGMA root (13-epochs)" = "#E07E5E", "UPGMA - SciPhy" = "yellow")
+alphas <- c("SciPhy constant" = 0.3, "UPGMA origin" = 1.0,"UPGMA root (constant)" = 1.0,"SciPhy MCC" = 1.0,"SciPhy dynamic" = 0.3, "UPGMA" = 1.0,"UPGMA root (13-epochs)" = 1.0, "UPGMA - SciPhy" = 0.3)
+all_ltt$type = factor(all_ltt$type, levels=c("SciPhy constant", "SciPhy dynamic","UPGMA origin", "UPGMA - SciPhy", "SciPhy MCC"))
 
 ltt_all <-  ggplot(all_ltt_constant,aes(x=time,y=N,group=number,colour=type,alpha = type)) + 
   geom_step() + 
@@ -240,31 +252,42 @@ typewriter <- read.table(typewriter_file, header = T) %>% slice_tail(prop = 0.10
 bd_rates <- data.frame(growthRate = typewriter[,"birthRate"] - typewriter[,"deathRate"])
 bd_rates$tree = "UPGMA origin"
 
-growth_rates = melt(bd_rates, id.vars = "tree")
+bd_rates = melt(bd_rates, id.vars = "tree")
+
+growth_rates = bd_rates
 
 ## Add UPGMA scaled based estimates
-typewriter_file <- "1000_UPGMA_medianPosteriorHeight.1707410740300.log"
-typewriter <- read.table(typewriter_file, header = T) %>% slice_tail(prop = 0.10)
-bd_rates <- data.frame(growthRate = typewriter[,"birthRate"] - typewriter[,"deathRate"])
-bd_rates$tree = "UPGMA root"
-bd_rates = melt(bd_rates, id.vars = "tree")
+#typewriter_file <- "1000_UPGMA_medianPosteriorHeight.1707410740300.log"
+#typewriter <- read.table(typewriter_file, header = T) %>% slice_tail(prop = 0.10)
+#bd_rates <- data.frame(growthRate = typewriter[,"birthRate"] - typewriter[,"deathRate"])
+#bd_rates$tree = "UPGMA root"
+#bd_rates = melt(bd_rates, id.vars = "tree")
 
-growth_rates = rbind(growth_rates, bd_rates)
+#growth_rates = rbind(growth_rates, bd_rates)
 
 ## Add the MCC tree based estimates
-typewriter_file <- "1000_MCC.1707412619101.log"
-typewriter <- read.table(typewriter_file, header = T) %>% slice_tail(prop = 0.10)
-bd_rates <- data.frame(growthRate = typewriter[,"birthRate"] - typewriter[,"deathRate"])
-bd_rates$tree = "SciPhy MCC"
-bd_rates = melt(bd_rates, id.vars = "tree")
+#typewriter_file <- "1000_MCC.1707412619101.log"
+#typewriter <- read.table(typewriter_file, header = T) %>% slice_tail(prop = 0.10)
+#bd_rates <- data.frame(growthRate = typewriter[,"birthRate"] - typewriter[,"deathRate"])
+#bd_rates$tree = "SciPhy MCC"
+#bd_rates = melt(bd_rates, id.vars = "tree")
 
-growth_rates = rbind(growth_rates, bd_rates)
+#growth_rates = rbind(growth_rates, bd_rates)
 
 ## Add the main analysis estimates
 typewriter_file <- "../../inference_results/clock_per_target/1000_cells/combined.log"
 typewriter <- read.table(typewriter_file, header = T) %>% slice_tail(prop = 0.10)
 bd_rates <- data.frame(growthRate = typewriter[,"birthRate"] - typewriter[,"deathRate"])
 bd_rates$tree = "SciPhy posterior"
+bd_rates = melt(bd_rates, id.vars = "tree")
+
+growth_rates = rbind(growth_rates, bd_rates)
+
+## Add the main analysis estimates
+typewriter_file <- "UPGMA_reheight.log"
+typewriter <- read.table(typewriter_file, header = T) %>% slice_tail(prop = 0.10)
+bd_rates <- data.frame(growthRate = typewriter[,"birthRate"] - typewriter[,"deathRate"])
+bd_rates$tree = "UPGMA - SciPhy"
 bd_rates = melt(bd_rates, id.vars = "tree")
 
 growth_rates = rbind(growth_rates, bd_rates)
@@ -277,9 +300,9 @@ prior =melt(prior, id.vars = "tree")
 growth_rates = rbind(growth_rates, prior)
 
 # Define order
-growth_rates$tree = factor(growth_rates$tree, levels=c("Prior", "SciPhy posterior", "SciPhy MCC", "UPGMA origin", "UPGMA root"))
+growth_rates$tree = factor(growth_rates$tree, levels=c("Prior", "SciPhy posterior", "UPGMA origin", "UPGMA - SciPhy"))
 #define colors
-cols <- c("Prior" = "white","SciPhy posterior" = "#E1E1F7", "UPGMA origin" = "red","UPGMA root" = "#E07E5E","SciPhy MCC" = "grey")
+cols <- c("Prior" = "white","SciPhy posterior" = "#56996E", "UPGMA origin" = "red","UPGMA root" = "#E07E5E","SciPhy MCC" = "grey","UPGMA - SciPhy" = "yellow")
 p_growth <- ggplot(growth_rates, aes(x=tree, y=value, fill = tree)) +
   theme_bw() +
   geom_violin(draw_quantiles = c(0.5))+  
@@ -297,31 +320,32 @@ p_growth <- ggplot(growth_rates, aes(x=tree, y=value, fill = tree)) +
 timeline_changes <- seq(0,25,by=25/13)
 
 #load the log file with 3 change points in the estimated rates
-log_file <- "1000_MCC.combined.log"
-typewriter <- read.table(log_file, header = T)
-typewriter_mcmc <- as.mcmc(typewriter)
-growth <- typewriter_mcmc[,paste0("birthRate.",1:13)] - typewriter_mcmc[,paste0("deathRate.",1:13)]
-growth_hpd = get_median_and_hpd(growth)
-growth_hpd[14, ] =  growth_hpd[13, ]# add last timepoint, st for time - 11 the same value remains
-growth_hpd$t = timeline_changes
-growth_hpd$tree = "SciPhy MCC"
+#log_file <- "1000_MCC.combined.log"
+#typewriter <- read.table(log_file, header = T)
+#typewriter_mcmc <- as.mcmc(typewriter)
+#growth <- typewriter_mcmc[,paste0("birthRate.",1:13)] - typewriter_mcmc[,paste0("deathRate.",1:13)]
+#growth_hpd = get_median_and_hpd(growth)
+#growth_hpd[14, ] =  growth_hpd[13, ]# add last timepoint, st for time - 11 the same value remains
+#growth_hpd$t = timeline_changes
+#growth_hpd$tree = "SciPhy MCC"
 
-growth_combined = growth_hpd
+#growth_combined = growth_hpd
 
 #load the log file with 3 change points in the estimated rates
-log_file <- "1000_UPGMA_medianPosteriorHeight.combined.log"
-typewriter <- read.table(log_file, header = T)
-typewriter_mcmc <- as.mcmc(typewriter)
-growth <- typewriter_mcmc[,paste0("birthRate.",1:13)] - typewriter_mcmc[,paste0("deathRate.",1:13)]
-growth_hpd = get_median_and_hpd(growth)
-growth_hpd[14, ] =  growth_hpd[13, ]# add last timepoint, st for time - 11 the same value remains
-growth_hpd$t = timeline_changes
-growth_hpd$tree = "UPGMA root"
+#log_file <- "1000_UPGMA_medianPosteriorHeight.combined.log"
+#typewriter <- read.table(log_file, header = T)
+#typewriter_mcmc <- as.mcmc(typewriter)
+#growth <- typewriter_mcmc[,paste0("birthRate.",1:13)] - typewriter_mcmc[,paste0("deathRate.",1:13)]
+#growth_hpd = get_median_and_hpd(growth)
+#growth_hpd[14, ] =  growth_hpd[13, ]# add last timepoint, st for time - 11 the same value remains
+#growth_hpd$t = timeline_changes
+#growth_hpd$tree = "UPGMA root"
 
-growth_combined = rbind(growth_combined, growth_hpd)
+#growth_combined = rbind(growth_combined, growth_hpd)
 
 
 #load the log file with 2 change points in the estimated rates
+
 log_file <- "1000_UPGMA.combined.log"
 typewriter <- read.table(log_file, header = T)
 typewriter_mcmc <- as.mcmc(typewriter)
@@ -330,26 +354,49 @@ growth_hpd = get_median_and_hpd(growth)
 growth_hpd[14, ] =  growth_hpd[13, ]# add last timepoint, st for time - 11 the same value remains
 growth_hpd$t = timeline_changes
 growth_hpd$tree = "UPGMA origin"
+growth_combined =  growth_hpd
+
+
+#load the log file with 2 change points in the estimated rates
+log_file <- "~/typewriter_analysis/results/analysis_cell_culture_data/inference_results/clock_per_target_OU/combined_burnin10.log"
+typewriter <- read.table(log_file, header = T)
+typewriter_mcmc <- as.mcmc(typewriter)
+growth <- typewriter_mcmc[,paste0("birthRate.",1:13)] - typewriter_mcmc[,paste0("deathRate.",1:13)]
+growth_hpd = get_median_and_hpd(growth)
+growth_hpd[14, ] =  growth_hpd[13, ]# add last timepoint, st for time - 11 the same value remains
+growth_hpd$t = timeline_changes
+growth_hpd$tree = "SciPhy dynamic"
+growth_combined = rbind(growth_combined, growth_hpd)
+
+
+#load the log file with 2 change points in the estimated rates
+log_file <- "UPGMA_reheight_OU.log"
+typewriter <- read.table(log_file, header = T)
+typewriter_mcmc <- as.mcmc(typewriter)
+growth <- typewriter_mcmc[,paste0("birthRate.",1:13)] - typewriter_mcmc[,paste0("deathRate.",1:13)]
+growth_hpd = get_median_and_hpd(growth)
+growth_hpd[14, ] =  growth_hpd[13, ]# add last timepoint, st for time - 11 the same value remains
+growth_hpd$t = timeline_changes
+growth_hpd$tree = "UPGMA - SciPhy"
 growth_combined = rbind(growth_combined, growth_hpd)
 
 #set colors and fills manually
-cols <- c("Prior" = "white","SciPhy posterior" = "#E1E1F7", "UPGMA origin" = "red","UPGMA root" = "#E07E5E","SciPhy MCC" = "grey")
-cols_fill<- c("Prior" = "white","SciPhy posterior" = "lightgreen", "UPGMA origin" = "red","UPGMA root" = "brown","SciPhy MCC" = "grey")
-
+cols <- c("SciPhy dynamic" = "#A4A4F2", "UPGMA origin" = "red", "UPGMA - SciPhy" = "yellow")
+cols_fill<- c("SciPhy dynamic" = "#A4A4F2", "UPGMA origin" = "red", "UPGMA - SciPhy" = "yellow")
 p_growth_OU = ggplot(growth_combined, aes(x=t, y=median, col=tree)) +
   geom_step(size=1)+
-  geom_stepribbon(aes(ymin = hpd_low, ymax=hpd_up, fill=tree), linetype="dotted", alpha = 0.1)+
-  theme_bw() + scale_color_manual(values = cols) + scale_color_manual(values = cols_fill) + 
-  theme(legend.title=element_blank(), legend.position = c(0.27, 0.858)) +
+  geom_stepribbon(aes(ymin = hpd_low, ymax=hpd_up, fill=tree, col=tree), linetype="dotted", alpha = 0.1)+
+  theme_bw() + scale_color_manual(values = cols) + scale_fill_manual(values = cols_fill) + 
+  theme(legend.title=element_blank()) +
   ylab(expression("Dynamic growth rate [" * d^-1 * "]"))+
   xlab("Time [d]")
 
 legend <- get_legend(p_growth) 
 p_growth_OU <- p_growth_OU + theme(text=element_text(size = text_size),panel.border = element_blank() , axis.line = element_line() ,panel.grid = element_blank())
 
-row_3 <- cowplot::plot_grid(p_growth+ theme(legend.position = "none") ,p_growth_OU + theme(legend.position = "none"),legend,ncol=3,nrow=1, rel_widths = c(1.0,1.0,0.6),axis = "l")
+row_3 <- cowplot::plot_grid(p_growth+ theme(legend.position = "none",panel.grid = element_blank(),axis.ticks.x = element_blank(),panel.border = element_blank(),axis.text.x = element_blank(),axis.line.x = element_line("black"),axis.line.y = element_line("black")) ,p_growth_OU + theme(legend.position = "none"),ncol=2,nrow=1, rel_widths = c(1.0,1.0),axis = "l")
 
-ggsave(paste0(pic_dir,"row_3.pdf"),full_figure, width = 14.28, height = 7.14, units = "cm", dpi = 300)
+ggsave(paste0(pic_dir,"row_3.pdf"),row_3, width = 14.28, height = 7.14, units = "cm", dpi = 300)
 
 ####------------------------------------
 #
