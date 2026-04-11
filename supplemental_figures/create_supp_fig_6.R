3e63e120e218bf27a5079cb2b1c2723a3cf59032
azwaans <antoine.zwaans@bsse.ethz.ch>
2026-04-11 19:17:18 +0200

    [Supp. figs 8-12] updated script with CCD tree

--- supplemental_figures/create_supp_fig_6.R
+++ supplemental_figures/create_supp_fig_6.R
@@ -1,6 +1,6 @@
 ## ---------------------------
 ##
-## Script name: plot_treespace
+## Script name: create_supp_fig_12
 ##
 ## Purpose of script: run treespace to calculate and plot wRF distances between SciPhy and UPGMA
 ##
@@ -26,9 +26,31 @@ label_size = 12
 require(tidyverse)
 require(data.table)
 library(treespace)
+library(phytools)
 
 ##sourcing helper functions
-source("plot_trees_in_2d_with_likelihoods.R")
+plot_tree_distances_branch_lengths_metric <- function(tree_df,tree_likelihood,metric_name,dim1,dim2) {
+  tree_df <-  tree_df
+  plot_title = paste("Weigthed Robinson Foulds","metric space")
+  dim_1 <- paste0("A",dim1)
+  dim_2 <- paste0("A",dim2)
+  
+  plot <- ggplot(tree_df[1: (length(tree_df$A1) - 3),], aes_string(x = dim_1, y = dim_2)) +
+    geom_point(aes(col = tree_likelihood), size = 0.4, alpha = 0.5) + 
+    scale_color_gradient(low = "white", high = "#05290B",name = "Log-likelihood")  +  
+    xlab("") + ylab("") + theme_bw(base_family = "")+ 
+    theme(legend.position = c(0.8,0.2), text = element_text(size = 10))+
+    geom_point(data=tree_df[(nrow(tree_df)):(nrow(tree_df)),], aes_string(x=dim_1,y=dim_2),colour="orange",size = 2.5)+ 
+    geom_text(data=tree_df[(nrow(tree_df)):(nrow(tree_df)),], aes_string(x=dim_1,y=dim_2),label="UPGMA root",hjust = 0.3,vjust =-0.2,size=2.5) +
+    geom_point(data=tree_df[(nrow(tree_df) - 2):(nrow(tree_df) - 2),], aes_string(x=dim_1,y=dim_2),colour="red",size = 2.5)+ 
+    geom_text(data=tree_df[(nrow(tree_df) - 2):(nrow(tree_df) - 2),], aes_string(x=dim_1,y=dim_2),label="UPGMA",hjust = 0.2,vjust =1,size=2.5)+ 
+    geom_point(data=tree_df[(nrow(tree_df) - 1):(nrow(tree_df) - 1),], aes_string(x=dim_1,y=dim_2),colour="black",size = 2.5)+ 
+    geom_text(data=tree_df[(nrow(tree_df) - 1):(nrow(tree_df) - 1),], aes_string(x=dim_1,y=dim_2),label="CCD",hjust = -0.2,vjust =1,size=2.5) +
+    ggtitle(plot_title) + theme(axis.text = element_text(size=6))
+  
+  
+  return(plot)
+}
 
 ## Load tree data and extract sample nbrs
 sciphy_trees <- ape::read.nexus(file = "../figure_4/inference_output/clockPerTarget_sampling_DataSet1_3000000.trees")
@@ -68,12 +90,12 @@ upgma$tip.label <- as.character(cell_ids_sorted$numeric_label)
 #create a upgma scaled by the median posterior tree height.
 median_posterior_height <- median(log[,"treeHeight.t.alignment"])
 upgma_rescaled <- upgma
-upgma_height <- tree_height_calc(upgma)
+upgma_height <- max(nodeHeights(upgma))
 upgma_rescaled$edge.length <- upgma_rescaled$edge.length * (median_posterior_height/upgma_height)
 
 #get the MCC tree 
-MCC <- ape::read.nexus(file = "../figure_4/inference_output/MCC_median_heights_clockPerTarget_sampling_DataSet1_3000000.tree")
-UPGMA_SCIPHY <- ape::read.nexus(file = "../figure_4/inference_output/combined_1000_UPGMA_medianPosteriorHeight_estimateBranchLengths_infer_rho_sampling_MCC.tree")
+MCC <- ape::read.nexus(file = "../figure_4/inference_output/CCD_clockPerTarget_sampling_DataSet1_3000000.tree")
+#UPGMA_SCIPHY <- ape::read.nexus(file = "../figure_4/inference_output/CCD_UPGMA_SciPhy.tree")
 
 #create a list of trees to analyse
 all_trees <- sciphy_trees
@@ -83,6 +105,8 @@ all_trees <-c(all_trees, upgma)
 all_trees <-c(all_trees, MCC) 
 all_trees <- c(all_trees,upgma_rescaled) 
 
+
+
 ## ---------------------------------------------------------------------
 ## Place all all_trees in 2d using the weighted Robinson Foulds (wRF) metric
 ## ---------------------------------------------------------------------
@@ -91,14 +115,38 @@ res_wrf <- treespace(all_trees, nf = 12, method = "wRF")
 
 # Plot wRF scenario
 tree_df_wrf <- res_wrf$pco$li
-plot1_2 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weigthed Robinson Foulds",1,2) + theme(axis.text = element_blank(),axis.ticks = element_blank(), legend.position ="none") + xlab("Coordinate 1") + ylab("Coordinate 2")
-plot1_3 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weigthed Robinson Foulds",1,3) + theme(plot.title = element_blank(), axis.text = element_blank(),axis.ticks = element_blank(), legend.position ="none") + xlab("Coordinate 1") + ylab("Coordinate 3") 
-plot1_4 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weigthed Robinson Foulds",1,4) + theme(plot.title = element_blank(), axis.text = element_blank(),axis.ticks = element_blank(), legend.position ="none") + xlab("Coordinate 1") + ylab("Coordinate 4")
-plot2_3 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weigthed Robinson Foulds",2,3) + theme(plot.title = element_blank(), axis.text = element_blank(),axis.ticks = element_blank(), legend.position ="none") + xlab("Coordinate 2") + ylab("Coordinate 3")
-plot2_4 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weigthed Robinson Foulds",2,4) + theme(plot.title = element_blank(), axis.text = element_blank(),axis.ticks = element_blank(), legend.position ="none") + xlab("Coordinate 2") + ylab("Coordinate 4")
-plot3_4 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weigthed Robinson Foulds",3,4) + theme(plot.title = element_blank(), axis.text = element_blank(),axis.ticks = element_blank() ) + xlab("Coordinate 3") + ylab("Coordinate 4")
+write.csv(res_wrf$pco$li,"supp_fig_12.csv")
+
+plot1_2 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weighted Robinson Foulds",1,2) + xlab("Coordinate 1") + ylab("Coordinate 2") + theme(legend.position = "None", panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+plot1_3 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weighted Robinson Foulds",1,3) + xlab("Coordinate 1") + ylab("Coordinate 3") + theme(legend.position = "None",panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+plot1_4 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weighted Robinson Foulds",1,4) + xlab("Coordinate 1") + ylab("Coordinate 4") + theme(legend.position = "None",panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+plot2_3 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weighted Robinson Foulds",2,3) + xlab("Coordinate 2") + ylab("Coordinate 3") + theme(legend.position = "None",panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+plot2_4 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weighted Robinson Foulds",2,4) + xlab("Coordinate 2") + ylab("Coordinate 4") + theme(legend.position = "None",panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+plot3_4 <- plot_tree_distances_branch_lengths_metric(tree_df_wrf, tree_likelihood, "Weighted Robinson Foulds",3,4) + xlab("Coordinate 3") + ylab("Coordinate 4") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 blank_plot <- ggplot() + theme_void()
-combined <- cowplot::plot_grid(plot1_2,plot1_3,plot1_4,blank_plot,plot2_3,plot2_4,blank_plot,blank_plot,plot3_4,nrow = 3,ncol=3)
-ggsave(paste0(pic_dir,"supp_fig_6.png"), combined, width = 60, height = 60, units = "cm", dpi = 300)
+legend <- get_legend(plot3_4) 
+combined <- cowplot::plot_grid(plot1_2+ theme(plot.title = element_blank()),plot1_3 + theme(plot.title = element_blank()),plot1_4+ theme(plot.title = element_blank()),blank_plot,plot2_3 + theme(plot.title = element_blank()),plot2_4 + theme(plot.title = element_blank()),legend,blank_plot,plot3_4+ theme(plot.title = element_blank(),legend.position = "None"),nrow = 3,ncol=3)
+
+#plot with titles
+title <- cowplot::ggdraw() + 
+  cowplot::draw_label(
+    "Weighted Robinson Foulds metric space",
+    fontface = 'bold',size = 10,
+    x = 0,
+    hjust = 0
+  ) +
+  theme(
+    # add margin on the left of the drawing canvas,
+    # so title is aligned with left edge of first plot
+    plot.margin = margin(0, 0, 0, 7)
+  )
+combined_with_title <- cowplot::plot_grid(
+  title, combined,
+  ncol = 1,
+  # rel_heights values control vertical title margins
+  rel_heights = c(0.1, 1)
+)
+
+ggsave(paste0(pic_dir,"supp_fig_12.pdf"),combined_with_title, width = 14.28, height = 14.28, units = "cm", dpi = 300)
 
--- supplemental_figures/create_supp_figs_3_4_5.R
+++ supplemental_figures/create_supp_figs_3_4_5.R
@@ -1,6 +1,6 @@
 ## ---------------------------
 ##
-## Script name: plot_TreeDist 
+## Script name: create_supp_fig_8_9_10_11
 ##
 ## Purpose of script: Plot outputs of 2D mapping obtained with TreeDist and assess quality.
 ## Relies on outputs from run_TreeDist_XX.R, unmapped distances and mapped distance run on cluster.
@@ -14,6 +14,9 @@
 ##
 ## -----------------------------
 
+## set directory
+
+setwd("/Users/azwaans/typewriter_analysis/paper_figures/figure_4/")
 
 ## set output directory for the plot
 pic_dir = "plots/"
@@ -36,7 +39,25 @@ library(gdata)
 #########################################################
 
 ##sourcing helper functions
-source("plot_trees_in_2d_with_likelihoods.R")
+plot_tree_distances_topology_metric <- function(tree_df,tree_likelihood,metric_name,dim1,dim2) {
+  tree_df <-  tree_df
+  plot_title = paste(metric_name,"metric space")
+  dim_1 <- paste0("A",dim1)
+  dim_2 <- paste0("A",dim2)
+  
+  plot <- ggplot(tree_df[1: (nrow(tree_df) - 2),], aes_string(x = dim_1, y = dim_2)) +
+    geom_point(aes(col = tree_likelihood), size = 0.4, alpha = 0.5) + 
+    scale_color_gradient(low = "white", high = "#05290B",name = "Log-likelihood") + 
+    xlab(dim_1) + ylab(dim_2) + theme_bw(base_family = "")+ 
+    theme(legend.position = c(0.6,1.43), text = element_text(size = 10)) +
+    geom_point(data=tree_df[(nrow(tree_df) - 1):(nrow(tree_df) - 1),], aes_string(x=dim_1,y=dim_2),colour="red",size = 2)+ 
+    geom_text(data=tree_df[(nrow(tree_df) - 1):(nrow(tree_df) - 1),], aes_string(x=dim_1,y=dim_2),label="UPGMA",hjust = -0.2,vjust =1,size=2.5)+ 
+    geom_point(data=tree_df[(nrow(tree_df)):(nrow(tree_df)),], aes_string(x=dim_1,y=dim_2),colour="black",size = 2)+ 
+    geom_text(data=tree_df[(nrow(tree_df)):(nrow(tree_df)),], aes_string(x=dim_1,y=dim_2),label="CCD",hjust = -0.3,vjust =1,size=2.5) +
+    ggtitle(plot_title) + theme(axis.text = element_text(size=6))
+  
+  return(plot)
+}
 
 #### preprocess log to extract the correct likelihood values to plot ####
 sciphy_trees <- ape::read.nexus(file = "../figure_4/inference_output/clockPerTarget_sampling_DataSet1_3000000.trees")
@@ -66,14 +87,14 @@ tree_likelihood <- subsampled_log$likelihood
 ############################
 
 #input RF 2D mapping 
-RF_treeDist <- read.csv( file = "../figure_4/inference_output/mapping_df_RF.csv")
+RF_treeDist <- read.csv( file = "../figure_4/inference_output/mapping_df_RF_CCD.csv")
 
 #input original distances
-distances <- read.csv( file = "../figure_4/inference_output/distances_RF.csv")
+distances <- read.csv( file = "../figure_4/inference_output/distances_RF_CCD.csv")
 
 #reformat into lowertriangular matrix
 distances <- unlist(distances)
-mat <- matrix(NA, ncol=1209, nrow=1209) 
+mat <- matrix(NA, ncol=length(sciphy_trees) +2, nrow=length(sciphy_trees) +2) 
 lowerTriangle(mat, diag=FALSE, byrow=FALSE) <- distances
 dist <- as.dist(mat)
 #calculate trustworthyness x continuity (mapping quality)
@@ -84,7 +105,7 @@ txc <- vapply(seq_len(ncol(RF_treeDist)), function(k) {
 plot(txc, xlab = "Dimension")
 
 #plot the mapping quality
-png(paste0(pic_dir,"mapping_quality_RF_MCC.png"))
+png(paste0(pic_dir,"supp_fig_11a.png"),width = 14.28, height = 14.28, units = "cm", res = 300)
 plot(txc, xlab = "Dimension")
 abline(h = 0.9, lty = 2)
 dev.off()
@@ -98,13 +119,14 @@ plot2_4_RF <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "
 plot3_4_RF <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Robinson Foulds",3,4) + xlab("Coordinate 3") + ylab("Coordinate 4") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 blank_plot <- ggplot() + theme_void()
-combined <- cowplot::plot_grid(plot1_2_RF+ theme(plot.title = element_blank()),plot1_3_RF + theme(plot.title = element_blank()),plot1_4_RF+ theme(plot.title = element_blank()),blank_plot,plot2_3_RF + theme(plot.title = element_blank()),plot2_4_RF + theme(plot.title = element_blank()),blank_plot,blank_plot,plot3_4_RF+ theme(plot.title = element_blank()),nrow = 3,ncol=3)
+legend <- get_legend(plot3_4_RF) 
+combined <- cowplot::plot_grid(plot1_2_RF+ theme(plot.title = element_blank()),plot1_3_RF + theme(plot.title = element_blank()),plot1_4_RF+ theme(plot.title = element_blank()),blank_plot,plot2_3_RF + theme(plot.title = element_blank()),plot2_4_RF + theme(plot.title = element_blank()),legend,blank_plot,plot3_4_RF+ theme(plot.title = element_blank(),legend.position = "None"),nrow = 3,ncol=3)
 
 #plot with titles
 title <- cowplot::ggdraw() + 
   cowplot::draw_label(
     "Robinson Foulds metric space",
-    fontface = 'bold',size = 25,
+    fontface = 'bold',size = 10,
     x = 0,
     hjust = 0
   ) +
@@ -120,20 +142,20 @@ combined_with_title <- cowplot::plot_grid(
   rel_heights = c(0.1, 1)
 )
 
-ggsave(paste0(pic_dir,"supp_fig_3.png"), combined_with_title, width = 60, height = 60, units = "cm", dpi = 300)
+ggsave(paste0(pic_dir,"supp_fig_8.pdf"), combined_with_title, width = 14.28, height = 14.28, units = "cm", dpi = 300)
 
 ###########################################
 ## Plotting in 2D Clustering Information ##
 ###########################################
 
-CI_treeDist <- read.csv( file = "../figure_4/inference_output/mapping_df_CI.csv")
+CI_treeDist <- read.csv( file = "../figure_4/inference_output/mapping_df_CI_CCD.csv")
 
 #input original distances
-distances <- read.csv( file = "../figure_4/inference_output/distances_CI.csv")
+distances <- read.csv( file = "../figure_4/inference_output/distances_CI_CCD.csv")
 
 #reformat into lowertriangular matrix
 distances <- unlist(distances)
-mat <- matrix(NA, ncol=1209, nrow=1209) 
+mat <- matrix(NA, ncol=length(sciphy_trees) +2, nrow=length(sciphy_trees) +2) 
 lowerTriangle(mat, diag=FALSE, byrow=FALSE) <- distances
 dist <- as.dist(mat)
 
@@ -144,7 +166,7 @@ txc <- vapply(seq_len(ncol(CI_treeDist)), function(k) {
 }, 0)
 
 #plot the mapping quality
-png(paste0(pic_dir,"mapping_quality_CI.png"))
+png(paste0(pic_dir,"supp_fig_11b.png"),width = 14.28, height = 14.28, units = "cm", res = 300)
 plot(txc, xlab = "Dimension")
 abline(h = 0.9, lty = 2)
 dev.off()
@@ -160,13 +182,14 @@ plot2_4 <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Clu
 plot3_4 <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Clustering Information",3,4) + xlab("Coordinate 3") + ylab("Coordinate 4") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 blank_plot <- ggplot() + theme_void()
-combined <- cowplot::plot_grid(plot1_2+ theme(plot.title = element_blank()),plot1_3 + theme(plot.title = element_blank()),plot1_4+ theme(plot.title = element_blank()),blank_plot,plot2_3 + theme(plot.title = element_blank()),plot2_4 + theme(plot.title = element_blank()),blank_plot,blank_plot,plot3_4+ theme(plot.title = element_blank()),nrow = 3,ncol=3)
+legend <- get_legend(plot3_4) 
+combined <- cowplot::plot_grid(plot1_2+ theme(plot.title = element_blank()),plot1_3 + theme(plot.title = element_blank()),plot1_4+ theme(plot.title = element_blank()),blank_plot,plot2_3 + theme(plot.title = element_blank()),plot2_4 + theme(plot.title = element_blank()),legend,blank_plot,plot3_4+ theme(plot.title = element_blank(),legend.position = "None"),nrow = 3,ncol=3)
 
 #plot with titles
 title <- cowplot::ggdraw() + 
   cowplot::draw_label(
     "Clustering Information metric space",
-    fontface = 'bold',size = 25,
+    fontface = 'bold',size = 10,
     x = 0,
     hjust = 0
   ) +
@@ -182,21 +205,21 @@ combined_with_title <- cowplot::plot_grid(
   rel_heights = c(0.1, 1)
 )
 
-ggsave(paste0(pic_dir,"supp_fig_4.png"), combined_with_title, width = 60, height = 60, units = "cm", dpi = 300)
+ggsave(paste0(pic_dir,"supp_fig_9.pdf"),combined_with_title, width = 14.28, height = 14.28, units = "cm", dpi = 300)
 
 
 #############################################
 ## Plotting in 2D Phylogenetic Information ##
 #############################################
 
-PI_treeDist <- read.csv( file = "../figure_4/inference_output/mapping_df_PI.csv")
+PI_treeDist <- read.csv( file = "../figure_4/inference_output/mapping_df_PI_CCD.csv")
 
 #input original distances
-distances <- read.csv( file = "../figure_4/inference_output/distances_PI.csv")
+distances <- read.csv( file = "../figure_4/inference_output/distances_PI_CCD.csv")
 
 #reformat into lowertriangular matrix
 distances <- unlist(distances)
-mat <- matrix(NA, ncol=1209, nrow=1209) 
+mat <- matrix(NA, ncol=length(sciphy_trees) +2, nrow=length(sciphy_trees) +2) 
 lowerTriangle(mat, diag=FALSE, byrow=FALSE) <- distances
 dist <- as.dist(mat)
 
@@ -207,7 +230,7 @@ txc <- vapply(seq_len(ncol(PI_treeDist)), function(k) {
 }, 0)
 
 #plot the mapping quality
-png(paste0(pic_dir,"mapping_quality_PI.png"))
+png(paste0(pic_dir,"supp_fig_11c.png"),width = 14.28, height = 14.28, units = "cm", res = 300)
 plot(txc, xlab = "Dimension")
 abline(h = 0.9, lty = 2)
 dev.off()
@@ -218,20 +241,20 @@ tree_df_rf <- PI_treeDist
 plot1_2 <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Phylogenetic Information",1,2) + xlab("Coordinate 1") + ylab("Coordinate 2") + theme(legend.position = "None", panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 plot1_3 <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Phylogenetic Information",1,3) + xlab("Coordinate 1") + ylab("Coordinate 3") + theme(legend.position = "None",panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 plot1_4 <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Phylogenetic Information",1,4) + xlab("Coordinate 1") + ylab("Coordinate 4") + theme(legend.position = "None",panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
 plot2_3 <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Phylogenetic Information",2,3) + xlab("Coordinate 2") + ylab("Coordinate 3") + theme(legend.position = "None",panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 plot2_4 <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Phylogenetic Information",2,4) + xlab("Coordinate 2") + ylab("Coordinate 4") + theme(legend.position = "None",panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 plot3_4 <- plot_tree_distances_topology_metric(tree_df_rf, tree_likelihood, "Phylogenetic Information",3,4) + xlab("Coordinate 3") + ylab("Coordinate 4") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 blank_plot <- ggplot() + theme_void()
-
-blank_plot <- ggplot() + theme_void()
-combined <- cowplot::plot_grid(plot1_2+ theme(plot.title = element_blank()),plot1_3 + theme(plot.title = element_blank()),plot1_4+ theme(plot.title = element_blank()),blank_plot,plot2_3 + theme(plot.title = element_blank()),plot2_4 + theme(plot.title = element_blank()),blank_plot,blank_plot,plot3_4+ theme(plot.title = element_blank()),nrow = 3,ncol=3)
+legend <- get_legend(plot3_4) 
+combined <- cowplot::plot_grid(plot1_2+ theme(plot.title = element_blank()),plot1_3 + theme(plot.title = element_blank()),plot1_4+ theme(plot.title = element_blank()),blank_plot,plot2_3 + theme(plot.title = element_blank()),plot2_4 + theme(plot.title = element_blank()),legend,blank_plot,plot3_4+ theme(plot.title = element_blank(),legend.position = "None"),nrow = 3,ncol=3)
 
 #plot with titles
 title <- cowplot::ggdraw() + 
   cowplot::draw_label(
     "Phylogenetic Information metric space",
-    fontface = 'bold',size = 25,
+    fontface = 'bold',size = 10,
     x = 0,
     hjust = 0
   ) +
@@ -247,5 +270,5 @@ combined_with_title <- cowplot::plot_grid(
   rel_heights = c(0.1, 1)
 )
 
-ggsave(paste0(pic_dir,"supp_fig_5.png"), combined_with_title, width = 60, height = 60, units = "cm", dpi = 300)
+ggsave(paste0(pic_dir,"supp_fig_10.pdf"),combined_with_title, width = 14.28, height = 14.28, units = "cm", dpi = 300)
 
